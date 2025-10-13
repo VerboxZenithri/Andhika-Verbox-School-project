39924674caebb6841aa794a22719219c0ea48dd2
VerboxZenithri <shafieandhika@gmail.com>
2025-10-13 13:12:54 +0700

    Add files via upload

--- Andhika (Verbox) School Python code/real peledak PC.py
+++ Andhika (Verbox) School Python code/real peledak PC.py
@@ -1,10 +1,10 @@
 def nuke(n):
     a = []
-    for i in range(10):
+    for i in range(100):
         if n > 1:
             a.append(nuke(n-1))
         else:
             a.append(i)
     return a
 
-print(nuke(10))
+print(nuke(15))
