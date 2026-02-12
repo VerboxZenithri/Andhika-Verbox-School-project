e0d2028fe2a26ad386a06c4a43bd9fff55b76fbf
VerboxZenithri <shafieandhika@gmail.com>
2026-02-12 11:22:30 +0700

    Add files via upload

--- Andhika (Verbox) School Python code/Switch/Match.py
+++ Andhika (Verbox) School Python code/Switch/Match.py
@@ -1,13 +1,21 @@
-command="exit"
-match command:
-    case "strt":
-        action="fire up the engine"
-    case "stop":
-        action="stop engine"
-    case "pause":
-        action="stop time"
-    case "exit":
-        action="explode de engine"
-    case _:
-        action="unknown command"
-print(action)
+Menu_Kantin = ["Cihu", "Batagor", "Kentang", "Es teh", "Jajan"]
+for food in Menu_Kantin:
+    print("==[Kantin]==")
+    print(food)
+Action = input("Istirahat makan apa aja? \n (Sebutkan nama makanan) : ")
+while True:
+    Pesanan = []
+    for Queue in Pesanan:
+        print(">[Pesanan]<")
+        print(Queue)
+    match Action:
+        case "Cihu":
+            Pesanan.append(Menu_Kantin[0])
+        case "Batagor":
+            Pesanan.append(Menu_Kantin[1])
+        case "Kentang":
+            Pesanan.append(Menu_Kantin[2])
+        case "Es teh":
+            Pesanan.append(Menu_Kantin[3])
+        case "Jajan":
+            Pesanan.append(Menu_Kantin[4])
