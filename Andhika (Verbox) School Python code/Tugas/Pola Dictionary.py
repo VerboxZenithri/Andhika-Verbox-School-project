985b5b2236532d2c56867671c6e4ee883f7559e1
VerboxZenithri <shafieandhika@gmail.com>
2026-02-13 23:52:33 +0700

    Add files via upload

---
+++ Andhika (Verbox) School Python code/Tugas/Pola Dictionary.py
@@ -0,0 +1,12 @@
+data={"role": "admin", "id": 1}
+
+match data:
+    case{"role": "admin", "id": id}:
+        act=f"Akses penuh untuk admin ID:{id}"
+    case{"role": "guest"}:
+        act="Akses terbatas untuk tamu"
+        
+    case _:
+        act="User tidak terdaftar"
+
+print(act)
